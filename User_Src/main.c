f4da86f6269a745aedb14bc04923a1372ba84b0f
Saoskywalker <773917760@qq.com>
2020-09-09 11:11:47 +0800

    fsfs

--- User_Src/main.c
+++ User_Src/main.c
@@ -37,7 +37,6 @@ int main()
 	TIM4_PWM_Init(7200, 0);		 //10Khz.
 	TIM_SetCompare3(TIM4, 3000); //4.6v
 	screenCheck();
-	delay_ms(1000);
 	dwStopMusic();
 	TIM3_Int_Init(99, 70); //100us
 	Adc_Init();
@@ -46,7 +45,7 @@ int main()
 	dwSetColor(DW_COL_RED, DW_COL_WHITE);
 	INLINE_MUSIC_START();
 	delay_ms(100);
-	delay_ms(5000);
+	delay_ms(5000);;;;;
 
 	STMFLASH_Read(FLASH_SAVE_ADDR+2, &j, 1); //mute state
 	muteFlag = j;
